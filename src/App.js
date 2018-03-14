6964d24642b26b641cd28b7df52e6e4eacd5d720
Anoop P V <anooppv567@gmail.com>
2018-03-14 21:27:36 +0530

    router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './Pages/Login/LoginPage';
-import route from 'react-router';
+import {Router, Route, Link, IndexRoute, hashHistory, browserHistory} from 'react-router';
 
 class App extends Component {
   render() {
     return (
-
-        <LoginPage/>
+    <Router history={hashHistory}>
+        <Route path='/' component={LoginPage} />
+        
+      </Router>
+        
       
     );
   }
